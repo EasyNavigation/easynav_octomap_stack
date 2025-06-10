c6d3116d1ddf61a10936f6358db69d31ba280ca1
estherag <e.aguado.glez@gmail.com>
2025-06-10 12:31:04 +0200

    clean

--- search/all_2.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['parameters_0',['Parameters',['../index.html#autotoc_md3',1,'']]]
-];
--- search/pages_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['easynav_5foctomap_5fstack_0',['easynav_octomap_stack',['../index.html',1,'']]]
-];
